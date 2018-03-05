fbc2423783ee770ce9089311db5aaa578013def6
Birdic <bidicradovan@gmail.com>
2018-03-05 17:08:08 +0100

    GUI for demo ver1.

--- gui_for_rpi_test.py
+++ gui_for_rpi_test.py
@@ -12,9 +12,10 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-pinsInit = [11, 13, 15, 35, 37]
+pinsInit = [11, 13, 15, 35]
 
 GPIO.setup(pinsInit, GPIO.OUT, initial = GPIO.LOW)
+GPIO.output(37, True)
 
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setwarnings(False)
@@ -49,7 +50,7 @@ def callback00():  # 1 & 7 ---- 1
     pinsHigh = [13, 15]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
     # GPIO.output(3, True)
@@ -65,7 +66,7 @@ def callback01():   # 2 & 7 ---- 3
     pinsHigh = [15]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback02():   # 3 & 7 ---- 5
@@ -76,7 +77,7 @@ def callback02():   # 3 & 7 ---- 5
     pinsHigh = [11, 15]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback03():   # 4 & 7 ---- 7
@@ -87,7 +88,7 @@ def callback03():   # 4 & 7 ---- 7
     pinsHigh = [11, 13, 15]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True) 
 
 def callback04():   # 1 & 9 ---- 9
@@ -98,7 +99,7 @@ def callback04():   # 1 & 9 ---- 9
     pinsHigh = [11, 13, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)  
 
 def callback05():   # 2 & 9 ---- 11
@@ -109,7 +110,7 @@ def callback05():   # 2 & 9 ---- 11
     pinsHigh = [11, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback06():   # 3 & 9 ---- 13
@@ -120,7 +121,7 @@ def callback06():   # 3 & 9 ---- 13
     pinsHigh = [35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback07():   # 4 & 9 ---- 15
@@ -131,7 +132,7 @@ def callback07():   # 4 & 9 ---- 15
     pinsHigh = [13, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback10():   # 1 & 8 ---- 2
@@ -142,7 +143,7 @@ def callback10():   # 1 & 8 ---- 2
     pinsHigh = [11]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback11():   # 2 & 8 ---- 4
@@ -151,7 +152,7 @@ def callback11():   # 2 & 8 ---- 4
     # GPIO.output(13, True)
     pinsLow = [11, 13, 15, 35, 37]
     GPIO.output(pinsLow, False)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback12():   # 3 & 8 ---- 6
@@ -162,7 +163,7 @@ def callback12():   # 3 & 8 ---- 6
     pinsHigh = [13]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback13():   # 4 & 8 ---- 8
@@ -173,7 +174,7 @@ def callback13():   # 4 & 8 ---- 8
     pinsHigh = [11, 13]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback14():   # 1 & 10 ---- 10
@@ -184,7 +185,7 @@ def callback14():   # 1 & 10 ---- 10
     pinsHigh = [11, 13, 15, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback15():   # 2 & 10 ---- 12
@@ -195,7 +196,7 @@ def callback15():   # 2 & 10 ---- 12
     pinsHigh = [11, 15, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback16():   # 3 & 10 ---- 14
@@ -206,7 +207,7 @@ def callback16():   # 3 & 10 ---- 14
     pinsHigh = [15, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callback17():   # 4 & 10 ---- 16
@@ -217,7 +218,7 @@ def callback17():   # 4 & 10 ---- 16
     pinsHigh = [13, 15, 35]
     GPIO.output(pinsLow, False)
     GPIO.output(pinsHigh, True)
-    time.sleep(10)
+    time.sleep(1)
     GPIO.output(37, True)
 
 def callbackSugarUp():  # 1 & 5
@@ -231,38 +232,39 @@ def callbackSugarDown(): # 1 & 6
 root = tk.Tk()
 sugarCounter = tk.IntVar()
 sugarCounter.set(2)
-root.resizable(width=False, height=False)
+#root.resizable(width=False, height=False)
+root.geometry("960x1080")
 root.configure(background="grey")
 # root.geometry("400x400") 
 
-drinkTextFont = tkFont.Font(family='Helvetica', size=8, weight=tkFont.BOLD)
-sugarTextFont = tkFont.Font(family='Helvetica', size=12, weight=tkFont.BOLD)
+drinkTextFont = tkFont.Font(family='Helvetica', size=12, weight=tkFont.BOLD)
+sugarTextFont = tkFont.Font(family='Helvetica', size=14, weight=tkFont.BOLD)
 photo00 = tk.PhotoImage(file="espresso38.gif")
 bckgrndBtn = '#CA9B00'
 
 labelInfoSugar = tk.Label(text="Pre nego što izaberete napitak regulišite količinu šećera sa - / +", font=sugarTextFont, wraplength=400, background="grey" )
 labelSugarLevel = tk.Label(textvariable=sugarCounter, font=sugarTextFont, background="grey")
-sugarBtnDown = tk.Button(height=1, width=3, text='-', borderwidth=5, font=drinkTextFont, command=callbackSugarDown)
-sugarBtnUp = tk.Button(height=1, width=3, text='+', borderwidth=5, font=drinkTextFont, command=callbackSugarUp)
+sugarBtnDown = tk.Button(height=1, width=4, text='-', borderwidth=5, font=drinkTextFont, command=callbackSugarDown)
+sugarBtnUp = tk.Button(height=1, width=4, text='+', borderwidth=5, font=drinkTextFont, command=callbackSugarUp)
 photoLabel = tk.Label(image = photo00)
-btn00 = tk.Button(text='Kratki espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback00)
-btn01 = tk.Button(text='Produženi espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback01)
-btn02 = tk.Button(text='Macchiato', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback02)
-btn03 = tk.Button(text='Cappucchino', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback03)
-btn04 = tk.Button(text='Cappucchino sa čokoladom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback04)
-btn05 = tk.Button(text='Nes espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback05)
-btn06 = tk.Button(text='Nes macchiato', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback06)
-btn07 = tk.Button(text='Nes cappucchino', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback07)
-btn10 = tk.Button(text='Topla čokolada', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback10)
-btn11 = tk.Button(text='Jača čokolada', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback11)
-btn12 = tk.Button(text='Čokolada sa mlekom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback12)
-btn13 = tk.Button(text='Čaj', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback13)
-btn14 = tk.Button(text='Mleko', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback14)
-btn15 = tk.Button(text='Bela kafa', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback15)
-btn16 = tk.Button(text='Prazna čaša', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback16)
-btn17 = tk.Button(text='Kratki espresso sa čokoladom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback17)
+btn00 = tk.Button(text='Kratki espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback00,  width = 1, height = 3)
+btn01 = tk.Button(text='Produženi espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback01,  width = 1, height = 3)
+btn02 = tk.Button(text='Macchiato', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback02,  width = 1, height = 3)
+btn03 = tk.Button(text='Cappucchino', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback03,  width = 1, height = 3)
+btn04 = tk.Button(text='Cappucchino sa čokoladom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback04,  width = 1, height = 3)
+btn05 = tk.Button(text='Nes espresso', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback05,  width = 1, height = 3)
+btn06 = tk.Button(text='Nes macchiato', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback06,  width = 1, height = 3)
+btn07 = tk.Button(text='Nes cappucchino', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback07,  width = 1, height = 3)
+btn10 = tk.Button(text='Topla čokolada', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback10,  width = 1, height = 3)
+btn11 = tk.Button(text='Jača čokolada', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback11,  width = 1, height = 3)
+btn12 = tk.Button(text='Čokolada sa mlekom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback12,  width = 1, height = 3)
+btn13 = tk.Button(text='Čaj', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback13,  width = 1, height = 3)
+btn14 = tk.Button(text='Mleko', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback14,  width = 1, height = 3)
+btn15 = tk.Button(text='Bela kafa', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback15,  width = 1, height = 3)
+btn16 = tk.Button(text='Prazna čaša', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback16,  width = 1, height = 3)
+btn17 = tk.Button(text='Kratki espresso sa čokoladom', compound=tk.LEFT, background=bckgrndBtn, font=drinkTextFont, borderwidth=7, command=callback17, width = 1, height = 3)
 
-root.rowconfigure((0,9), weight=1)  # make buttons stretch when
+root.rowconfigure((0,15), weight=1)  # make buttons stretch when
 root.columnconfigure((0,4), weight=1)  # when window is resized
 
 labelInfoSugar.grid(row=0, column=0, columnspan=5)
